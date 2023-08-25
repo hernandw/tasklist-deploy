92343ba4acee5de31930f7abeaa4c5ccb3503abc
Williams Hernández <hernandw@gmail.com>
2023-08-25 08:24:22 -0400

    location

--- client/src/components/TaskForm.jsx
+++ client/src/components/TaskForm.jsx
@@ -12,7 +12,7 @@ const TaskForm = () => {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(body),
       });
-      window.location = "/"
+      window.location = "https://taskliste.onrender.com/";
     } catch (error) {
       console.log(error)
     }
