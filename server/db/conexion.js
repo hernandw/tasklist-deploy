b8d44dae09224767705f5f442956bbf6e0fd0f7a
Williams Hernández <hernandw@gmail.com>
2023-08-24 20:49:38 -0400

    :bug: correcion de la conexion remota a render

--- server/db/conexion.js
+++ server/db/conexion.js
@@ -1,23 +1,28 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool = new Pool({
+/* const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
     allowExitOnIdle: true
+}) */
+
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL
 })
 
 /* const pool = new Pool({
     connectionString: process.env.DATABASE_URL
 }) */
 
-/* const getData = async () => {
-    const consulta = 'SELECT * FROM task';
+const getData = async () => {
+    const consulta = 'SELECT NOW()';
     const { rows } = await pool.query(consulta)
     console.log(rows)
 }
 
-getData() */
+getData()
 module.exports = pool
